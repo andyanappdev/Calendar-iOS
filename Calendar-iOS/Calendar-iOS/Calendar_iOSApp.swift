fe779c6eace3a093af321c827a6e4efbedb6f5f2
Andy An <andyanappdev@gmail.com>
2024-04-16 12:49:36 +0900

    config(*): Setup iOS project

---
+++ Calendar-iOS/Calendar-iOS/Calendar_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_iOSApp.swift
+//  Calendar-iOS
+//
+//  Created by Doyoung An on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
